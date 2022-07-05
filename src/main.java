08f3b63e4bdcae33f5355e4f42cc1b53250e048b
Serg5858 <ss3@ro.ru>
2022-07-05 22:36:15 +0300

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        int costOfTicket = 600;
+        int calculationOfMiles = 20;
+        int result = costOfTicket / calculationOfMiles;
+        System.out.println(result);
+    }
+
+}
