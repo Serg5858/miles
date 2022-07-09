4ba6b639158e907fe408f21508dbf8aabe53536b
Serg5858 <ss3@ro.ru>
2022-07-09 12:02:34 +0300

    miles

--- src/main.java
+++
@@ -1,9 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        int costOfTicket = 600;
-        int calculationOfMiles = 20;
-        int result = costOfTicket / calculationOfMiles;
-        System.out.println(result);
-    }
-
-}
